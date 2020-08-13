6434e98d698a48d5411a86849667920e39883033
vitz <viton007@op.pl>
2020-08-13 16:47:27 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,29 @@
+
+
+
+### init ###
+from ImgUploader import ImgUploader
+
+url = "http://www.vitz.usermd.net"
+user_key = "LXJdKWdqR2UEfaOcgLdAupYlFXY"
+sec_key = "KMEXnv5PTvmevfIsZbv5Igo7Guc"
+img_uploader = ImgUploader(url, user_key, sec_key)
+
+# upload
+for i in range(100):
+    print(i)
+    img_uploader.post_image("test.jpg")
+
+# get
+img = img_uploader.get_image(107)
+print(img)
+
+# get all
+imgs = img_uploader.get_images()
+print(imgs)
+
+# remove
+print(img_uploader.remove_image(109))
+
+# removel all
+print(img_uploader.remove_all())
